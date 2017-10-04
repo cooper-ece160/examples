7e1744649b5bf4d02115bc7f1c7eecdf1cdd5fec
cooper-ece160 <j_astrauckas@yahoo.com>
2017-10-04 18:25:37 -0400

    include arrays

--- sizeof_examples.c
+++ sizeof_examples.c
@@ -3,6 +3,14 @@
 
 int main(int argc, char *argv[])
 {
+	char s[] = "helloooooooooo";
+	char *sp = s;
+	
+	int s_size = sizeof s;
+	int sp_size = sizeof sp;
+	
+	printf("s_size: %d, sp_size: %d\n", s_size, sp_size);
+	
 	int long_size1 = sizeof(long int);
 	long l = 0;
 	int long_size2 = sizeof l;
